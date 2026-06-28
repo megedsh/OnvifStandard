01942730a39ca071e4205c1d3c6e338d0d75144b
megedsh <megedsh@gmail.com>
2026-06-28 14:13:26 +0300

    Update PtzTestingClient ServiceUri for testing

--- src/OnvifTests/PtzGetXmlResponseTests.cs
+++ src/OnvifTests/PtzGetXmlResponseTests.cs
@@ -223,7 +223,7 @@ namespace OnvifTests
         {
             PtzTestingClient ic = new PtzTestingClient
             {
-                ServiceUri = new Uri("http://10.5.23.102:80/onvif/ptz_service"),
+                ServiceUri = new Uri("http://192.168.3.19:8000/onvif/ptz_service"),
                 User = "root",
                 Password = "Q1w2e3r4",
                 SoapClient = new SoapClient()
